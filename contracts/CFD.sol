42414fc44b390bca92f566ba131e2d72f2f113a8
Haythem Sellami <sellamihaythem96@gmail.com>
2020-02-15 00:18:21 -0700

    init contracts

---
+++ contracts/CFD.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.5.5;
+
+contract CFD {
+
+    constructor() public {
+    }
+
+}
---
+++ contracts/DaiHard.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.5.5;
+
+contract DaiHard {
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+}
