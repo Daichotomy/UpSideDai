69a807256e380fc83c579947f36e5ced30121e5b
Christopher Maree <christopher.maree@gmail.com>
2020-02-15 16:58:07 -0700

    changes

--- src/components/ItterableComponents/PoolHistoryItem.vue
+++ src/components/ItterableComponents/PoolHistoryItem.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="page-container">
     <div class="rowObject">
+      <div class="md-layout" style="padding-left:40px;padding-top:15px">
+        <div class="md-layout-item">
+          <span class="rowDetails">DAI</span>
+        </div>
+        <div class="md-layout-item"></div>
+        <div class="md-layout-item"></div>
+        <div class="md-layout-item"></div>
+        <div class="md-layout-item"></div>
+      </div>
+
       {{ historyItem }}
     </div>
   </div>
@@ -35,5 +45,18 @@ export default {
   border: 1px solid rgba(0, 0, 0, 0.2);
   box-sizing: border-box;
   border-radius: 30px;
+}
+.rowDetails {
+  width: 80px;
+  height: 33px;
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 16px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  color: #c4c4c4;
 }
 </style>
