34b415af589f02acb9316115b6f611dd613091be
Haythem Sellami <sellamihaythem96@gmail.com>
2020-02-15 00:24:50 -0700

    token mock

--- contracts/DaiHard.sol
+++ contracts/DaiHard.sol
@@ -2,6 +2,8 @@ pragma solidity ^0.5.5;
 
 contract DaiHard {
 
+    address owner;
+
     constructor() public {
         owner = msg.sender;
     }
---
+++ contracts/mocks/TokenMock.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.5.5;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol";
+
+/**
+ * @notice ERC20 token mock
+ */
+contract TokenMock is ERC20Mintable {
+}
