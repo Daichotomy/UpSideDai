c32335653fc5abcaa854b7fd22c334084a4e9379
Haythem Sellami <sellamihaythem96@gmail.com>
2020-02-15 13:06:39 -0700

    clean test file

--- test/daihard.js
+++ test/daihard.js
@@ -1,17 +1,5 @@
-const {
-    ether,
-    BN
-} = require("openzeppelin-test-helpers");
-
 const BigNumber = require('bignumber.js');
-// const EVMRevert = require('./helpers/EVMRevert').EVMRevert;
-// const increaseTime = require('./helpers/increaseTime');
-// const increaseTimeTo = increaseTime.increaseTimeTo;
 
-const ZERO_ADDR = '0x0000000000000000000000000000000000000000';
-
-const TokenMock = artifacts.require("TokenMock");
-const MakerMedianizerMock = artifacts.require("MakerMedianizerMock");
 const DaiHard = artifacts.require("DaiHard");
 
 require('chai')
@@ -20,19 +8,10 @@ require('chai')
   .should();
 
 contract("DAI Hard", ([daiHardTeam, random]) => {
-    let dai, makerMedianizer, uniswapFactory, daiHard;
+    let daiHard;
 
     before(async() => {
-        // ERC20 token mock for testing
-        dai = await TokenMock.new(
-            "DAI",
-            "DAI",
-            18,
-            { from: daiHardTeam }
-        );
-
         daiHard = await DaiHard.new({ from: daiHardTeam });
-
     });
     
     describe("DaiHard deployment", async() => {
