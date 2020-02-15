4a493d05dfb1cbc1fc46400336ce5dfb3c89200f
Christopher Maree <christopher.maree@gmail.com>
2020-02-15 09:15:55 -0700

    daiprice

--- src/views/Trade.vue
+++ src/views/Trade.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="page-container">
-    trade page
+    <div class="md-layout sections">
+      <div class="md-layout-item">blank</div>
+      <div class="md-layout-item md-size-15">
+        <span class="currentPrice">Current Dai Price</span>
+        <br />
+        <br />
+        <div class="priceBlob">
+          <span class="priceBlobText">
+            {{ daiPrice }}
+          </span>
+        </div>
+      </div>
+      <div class="md-layout-item">section</div>
+      <div class="md-layout-item">blank</div>
+    </div>
   </div>
 </template>
 
@@ -12,7 +26,9 @@ export default {
   name: "trade",
   components: {},
   data() {
-    return {};
+    return {
+      daiPrice: 420.6969
+    };
   },
   methods: {},
   mounted() {}
@@ -21,4 +37,45 @@ export default {
 
 <style lang="scss" scoped>
 @import "../styles/variables.scss";
+
+.sections {
+  text-align: center;
+  padding-top: 100px;
+  padding-bottom: 100px;
+  vertical-align: middle;
+}
+.currentPrice {
+  font-weight: 300;
+  font-size: 18px;
+  line-height: 21px;
+}
+// .priceBlob {
+//   // position: absolute;
+//   width: 216px;
+//   height: 80px;
+//   left: 238px;
+//   top: 313px;
+//   background: rgba(254, 206, 77, 0.3);
+//   border-radius: 10px;
+// }
+
+.priceBlobText {
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: 300;
+  font-size: 45px;
+  line-height: 53px;
+  // display: flex;
+  align-items: center;
+  text-align: center;
+  padding: 10px;
+
+  color: rgba(0, 0, 0, 0.8);
+  width: 216px;
+  height: 80px;
+  left: 238px;
+  top: 313px;
+  background: rgba(254, 206, 77, 0.3);
+  border-radius: 10px;
+}
 </style>
