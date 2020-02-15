6ea87099ff8650ffeabf3b276aefea8ffdcd8169
Christopher Maree <christopher.maree@gmail.com>
2020-02-15 09:34:56 -0700

    additional styles

--- src/views/Trade.vue
+++ src/views/Trade.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="page-container">
     <div class="md-layout sections">
-      <div class="md-layout-item">blank</div>
-      <div class="md-layout-item md-size-15">
-        <span class="currentPrice">Current Dai Price</span>
+      <div class="md-layout-item"></div>
+      <div class="md-layout-item md-size-15" style="padding-top:75px">
+        <span class="infoText">Current Dai Price</span>
         <br />
         <br />
         <div class="priceBlob">
@@ -12,8 +12,25 @@
           </span>
         </div>
       </div>
-      <div class="md-layout-item">section</div>
-      <div class="md-layout-item">blank</div>
+      <div class="md-layout-item">
+        <div class="tradeBox">
+          <div class="md-layout tradeBoxItem">
+            <div class="md-layout-item" style="padding-top:70px">
+              <span class="infoText">Dai</span>
+              <br />
+              <span class="inputDai">
+                <md-field md-inline>
+                  <label>To Deposit...</label>
+                  <md-input v-model="inputDaiAmount" class="inputDai" type="number"></md-input>
+                </md-field>
+              </span>
+            </div>
+            <div class="md-layout-item">b</div>
+            <div class="md-layout-item">c</div>
+          </div>
+        </div>
+      </div>
+      <div class="md-layout-item"></div>
     </div>
   </div>
 </template>
@@ -27,7 +44,8 @@ export default {
   components: {},
   data() {
     return {
-      daiPrice: 420.6969
+      daiPrice: 420.69,
+      inputDaiAmount: 420.69
     };
   },
   methods: {},
@@ -44,20 +62,25 @@ export default {
   padding-bottom: 100px;
   vertical-align: middle;
 }
-.currentPrice {
+.sectionText {
+  font-weight: 300;
+  font-size: 24px;
+  line-height: 21px;
+}
+.infoText {
+  font-style: normal;
   font-weight: 300;
   font-size: 18px;
   line-height: 21px;
 }
-// .priceBlob {
-//   // position: absolute;
-//   width: 216px;
-//   height: 80px;
-//   left: 238px;
-//   top: 313px;
-//   background: rgba(254, 206, 77, 0.3);
-//   border-radius: 10px;
-// }
+.inputDai {
+  font-weight: 300;
+  font-size: 30px !important;
+  line-height: 47px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+}
 
 .priceBlobText {
   font-family: Roboto;
@@ -77,5 +100,19 @@ export default {
   top: 313px;
   background: rgba(254, 206, 77, 0.3);
   border-radius: 10px;
+}
+.tradeBox {
+  margin: 15px;
+  width: 694px;
+  height: 208px;
+  left: 508px;
+  top: 240px;
+  background: #ffffff;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  // box-sizing: border-box;
+  // box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.25);
+  border-radius: 30px;
+}
+.tradeBoxItem {
 }
 </style>
