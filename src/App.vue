8f59f0a3ba491f99635ddc2c5df7beda4ed0b0d9
Christopher Maree <christopher.maree@gmail.com>
2020-02-15 21:56:36 -0700

    menu

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,7 @@
           </li>
         </ul>
         <span
-          ><clickable-address style="margin-top:10px" :eth-address="account" />
+          ><clickable-address style="margin-top:20px" :eth-address="account" />
           <p style="padding-right:10px  padding-top:5px; margin-top:4px">
             {{ currentNetwork }}
           </p></span
